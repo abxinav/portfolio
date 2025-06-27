12dcf32bc3fd26a1d67b9e260316fa4780bbf073
abxinav <89390145+abxinav@users.noreply.github.com>
2025-06-27 11:11:56 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
+// Layout components
+import Layout from './components/layout/Layout';
+
+// Page components
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
+import Education from './pages/Education';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/experience" element={<Experience />} />
+            <Route path="/education" element={<Education />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </AnimatePresence>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
